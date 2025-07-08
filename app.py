4dc1b04e8d49be833049fdc5ee8086e865771d5a
Ahsan608 <arslanbutt9955@gmail.com>
2025-07-08 15:11:40 +0500

    Update app.py, add chat log and README

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
-
+history=[]
 
 temperature = 0.7
 top_p = 0.9
@@ -18,9 +18,25 @@ headers = {
     'X-goog-api-key': api_key
 }
 
+def save_chat_to_file():
+    with open(r"D:\task1\chat_log.txt","a") as f:
+        for hist in history:
+            f.write(hist[0]+" : ")
+            f.write(hist[1]+"\n")
+
+
+
 def send_message(message):
+    content=[]
+    history.append(("user",message))
+    for chat in history:
+        content.append({
+        "role":chat[0],
+        "parts": [{"text": chat[1]}]
+    })
+        
     payload = {
-        "contents": [{"parts": [{"text": message}]}],
+        "contents": content,
         "generationConfig": {
             "temperature": temperature,
             "topP": top_p,
@@ -34,9 +50,11 @@ def send_message(message):
         print(f"Error: {response.status_code} - {response.text}")
         return "API Error"
     data = response.json()
-    return data['candidates'][0]['content']['parts'][0]['text']
+    reply=data['candidates'][0]['content']['parts'][0]['text']
+    history.append(("model",reply))
+    return reply
 
-print("Gemini Chat - Type 'exit' to quit")
+print("Gemini Chat - Type 'exit' to save and quit ")
 print(f"Settings: temp={temperature}, top_p={top_p}, top_k={top_k}, max_tokens={max_output_tokens}")
 print("-" * 50)
 
@@ -44,6 +62,8 @@ while True:
     user_input = input("\nYou: ")
     
     if user_input.lower() == 'exit':
+        save_chat_to_file()
+        print("chats saved")
         break
-    
-    print("Gemini:", send_message(user_input))
+    else:
+        print("Gemini:", send_message(user_input))
